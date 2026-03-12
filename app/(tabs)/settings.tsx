ba1eacc2264a946ac5ca776f827e596ae78f63d2
Inba <inbavarunans@gmail.com>
2026-03-12 22:01:39 +0530

    feat:implemented settings page UI

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,26 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import useTheme from '@/hooks/useTheme';
+import { createSettingsStyles } from '@/assets/styles/settings.style';
+import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
+import ProgressStats from '@/components/ProgressStats';
 
 const Settings = () => {
+
+  const[isAutoSync, setIsAutoSync] = useState(true);
+  const[isNotificationEnabled, setIsNotification] = useState(true);
+
+
+  const {colors, isDarkMode, toggleDarkMode} = useTheme();
+
+  const settingsStyles = createSettingsStyles(colors);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Settings Page</Text>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={settingsStyles.container}>
+      <SafeAreaView style={settingsStyles.safeArea}>
+        {/* Header */}
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+            <LinearGradient colors={colors.gradients.primary} style={settingsStyles.iconContainer}>
+              <Ionicons name='settings' size={28} color="#ffffff" />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
+        </View>
+
+
+        <ScrollView style={settingsStyles.scrollView} contentContainerStyle={settingsStyles.content} showsVerticalScrollIndicator={false}>
+
+          <ProgressStats />
+        </ScrollView>
+      </SafeAreaView>
+      <Text>Settings Screen</Text>
+    </LinearGradient>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  text: {
-    color: "white",
-    fontFamily: "sans-serif",
-    fontSize: 18
-  }
-})
 
 export default Settings
